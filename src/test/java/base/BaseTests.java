7e60d53ceceb929636b35dd6d14661c72da9cf97
Edwin Casallas <casallas22@hotmail.com>
2021-07-09 02:06:57 -0500

    Se agregaron nuevos cambios

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,28 +5,38 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
+
     private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
     public  void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
+        homePage = new HomePage(driver);
 
-        WebElement inputsLink =  driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
-
-        System.out.println(driver.getTitle());
+    }
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
+
+
+
+
+/*WebElement inputsLink =  driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+        System.out.println(driver.getTitle());*/
